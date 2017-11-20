c999deb4f26ba30907ccb442f2863159111247b9
YanaVG <yanavg18@gmail.com>
2017-11-20 09:42:22 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+function addKeyboardLayout(alphabet) {
+	alphabet = prompt("Введите рядок-алфавіт" , '');
+		console.log(alphabet);
+}
+ 
+ function getRandCharInRow(row) {
+const row1 = "qwertyuiop";
+const row2 = "asdfghjkl";
+const row3 = "zxcvbnm";
+const chooseRow = 0;
+if (chooseRow = row1){
+	console.log(row1.Math.random);
+
+ }
+else if (chooseRow = row2){
+	console.log(row2.Math.random);
+ }
+else if (chooseRow = row3){
+	console.log(row3.Math.random);
+} else {
+	console.log("Choose row!")
+}
+}
+
+function getRandCharInAlph(letter) {
+	const alphabet = "zxcvbnmasdfghjklqwertyuiop";
+	letter = alphabet.Math.random;
+	console.log(letter);
+}
